a285dc4582f4614614e7e75764c43993431582c4
C. Eckenreiter <cckenreiter@gmail.com>
2024-02-29 15:12:54 -0500

    Created Components and Set FrameWork connecting them to app.js and index.js

---
+++ src/Left.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Left() {
+    return (
+        <div> 
+            <p>Testing</p>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+export default Left; 
---
+++ src/Right.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Right() {
+    return <p>Hello</p>; 
+}; 
+
+export default Right; 
