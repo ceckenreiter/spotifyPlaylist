2b66d8b01a54481ce2c869786003ac80f429ab24
C. Eckenreiter <cckenreiter@gmail.com>
2024-04-09 15:44:16 -0400

    extracted search data from api

--- src/SearchedResults.js
+++ src/SearchedResults.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import './ChangingBody'
 import './css/SearchedResults.css'
 import PlayButton from "./PlayButton";
@@ -6,8 +6,10 @@ import ViewButton from "./ViewButton";
 import AddToPlaylistButton from "./AddToPlaylistButton";
 
 
-function SearchedResults (props, NowPlaying, setNowPlaying, setAlbum, setArtist, Album, Artist, display, setDisplay, thisPlaylist, setThisPlaylist) { 
+function SearchedResults (props, searchInput, setSearchResults, searchResults) {
 
+    const [albumList, setAlbumList] = useState([])
+    const [trackList, setTrackList] = useState([])
 
     useEffect(() => {
         let q=props.searchInput.stringify
@@ -22,40 +24,28 @@ function SearchedResults (props, NowPlaying, setNowPlaying, setAlbum, setArtist,
         }
 
         fetch(`https://api.spotify.com/v1/search?q=${props.searchInput}&type=artist%2Calbum%2Ctrack`, authParam)
-        .then(response => {
-            console.log(response)
+        .then(response => response.json())
+        .then(result => { 
+            props.setSearchResults(result)
+            setAlbumList(result.albums.items)
+            console.log(albumList)
         })
         .catch(error => console.log(error))
     }, [props.searchInput])
    
-   
-   
-   
-   
     return (
         <div id='SearchedResults'>
-            <p>Results for: {props.searchInput}</p>
-            <div id='SongResults'>
-                <div className='List'> 
-                <p>Songs</p>
-                </div>
+            <p>Results for: {props.searchInput}</p> 
+            <div className="tracks">
+                    {albumList.map((item, index) => (
+                        <div key={index}>
+                            <p><a>{item.name}</a></p>
+                        </div>
+                    ))}  
             </div>
-
-            <div id='ArtistResults'>
-                <div className='List'>    
-                <p>Artists</p>     
-                     
-                </div>
-            </div>
-       
-            <div id='AlbumResults'>
-                <div className='List'>
-                    <p>Albums</p>
-                    
            
-                </div>
-            </div>
-    </div>
+            
+        </div>
     )
     }; 
 
