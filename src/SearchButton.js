179c6601b4a572cecee6c99dcf6ad124f3009afd
C. Eckenreiter <cckenreiter@gmail.com>
2024-04-09 14:33:08 -0400

    fetch call for searching added

--- src/SearchButton.js
+++ src/SearchButton.js
@@ -1,40 +1,18 @@
 import React from "react";
-import trackList from "./SpotifyAPI";
 import SearchedResults from "./SearchedResults";
 
 
 
 
-function SearchButton(props, display, setDisplay, NowPlaying, setNowPlaying, setAlbum, setArtist, Album, Artist, thisPlaylist, setThisPlaylist) {
+function SearchButton(props, display, setDisplay, searchInput) {
 
     const handleClick = (e) => {
         e.preventDefault();
-        const songList = trackList.filter((options) => options.song === props.searchInput);
-        
-        const artistList = trackList.filter((options) => options.artist === props.searchInput);
-        let artistIndex = artistList.findLastIndex((options)=>options.artist ===props.searchInput)
-        let checkedArtist = artistList.slice(artistIndex)
-       
-        const albumList = trackList.filter((options) => options.album === props.searchInput);
-        let albumIndex = albumList.findLastIndex((options)=>options.artist ===props.searchInput)
-        let checkedAlbum = albumList.slice(albumIndex)
-        
         props.setDisplay(
             <SearchedResults 
                 setDisplay={props.setDisplay}
                 display = {props.display}
                 searchInput={props.searchInput}
-                NowPlaying={props.NowPlaying} 
-                setNowPlaying={props.setNowPlaying} 
-                songList={songList} 
-                artistList={checkedArtist}
-                albumList={checkedAlbum}
-                Album={props.Album}
-                Artist={props.Artist}
-                setAlbum={props.setAlbum} 
-                setArtist={props.setArtist}
-                thisPlaylist = {props.thisPlaylist}
-                setThisPlaylist={props.setThisPlaylist}
             /> 
             )
     }
--- src/SearchedResults.js
+++ src/SearchedResults.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './ChangingBody'
 import './css/SearchedResults.css'
 import PlayButton from "./PlayButton";
@@ -7,103 +7,51 @@ import AddToPlaylistButton from "./AddToPlaylistButton";
 
 
 function SearchedResults (props, NowPlaying, setNowPlaying, setAlbum, setArtist, Album, Artist, display, setDisplay, thisPlaylist, setThisPlaylist) { 
+
+
+    useEffect(() => {
+        let q=props.searchInput.stringify
+        let token = window.localStorage.getItem('token')
+
+        var authParam = {
+            method: 'GET', 
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/x-www-form-urelencoded"
+            },
+        }
+
+        fetch(`https://api.spotify.com/v1/search?q=${props.searchInput}&type=artist%2Calbum%2Ctrack`, authParam)
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => console.log(error))
+    }, [props.searchInput])
+   
+   
+   
+   
+   
     return (
         <div id='SearchedResults'>
             <p>Results for: {props.searchInput}</p>
             <div id='SongResults'>
                 <div className='List'> 
                 <p>Songs</p>
-                    {props.songList.map((option, index) => (
-                            <div key={index}>
-                                <a>
-                                <span>{option.song}</span>
-                                <span>{option.album}</span>
-                                <span>{option.artist}</span>
-                                <PlayButton
-                                    value1={option.song}
-                                    value2={option.artist}
-                                    value3={option.album}
-                                    Album={props.Album}
-                                    setAlbum={props.setAlbum}
-                                    setArtist={props.setArtist}
-                                    Artist={props.Artist}
-                                    NowPlaying={props.NowPlaying}
-                                    setNowPlaying={props.setNowPlaying}
-                                />
-                                <AddToPlaylistButton 
-                                    value1={option.song}
-                                    value2={option.artist}
-                                    value3={option.album}
-                                    Album={props.Album}
-                                    setAlbum={props.setAlbum}
-                                    setArtist={props.setArtist}
-                                    Artist={props.Artist}
-                                    thisPlaylist={props.thisPlaylist}
-                                    setThisPlaylist={props.setThisPlaylist}
-                                    />
-
-                                </a>
-                            </div>
-                    ))}   
                 </div>
             </div>
 
             <div id='ArtistResults'>
                 <div className='List'>    
                 <p>Artists</p>     
-                    {props.artistList.map((option, index) => (
-                            <div key={index}>
-                                <a>
-                                <span>{option.artist}</span>
-                                <ViewButton 
-                                    value1={option.song}
-                                    value2={option.artist}
-                                    value3={option.album}
-                                    number={1}
-                                    value={option.artist}
-                                    display={props.display}
-                                    setDisplay={props.setDisplay}
-                                    Album={props.Album}
-                                    setAlbum={props.setAlbum}
-                                    setArtist={props.setArtist}
-                                    Artist={props.Artist}
-                                    NowPlaying={props.NowPlaying}
-                                    setNowPlaying={props.setNowPlaying}
-                                    thisPlaylist={props.thisPlaylist}
-                                    setThisPlaylist={props.setThisPlaylist}/>
-                                </a>
-                            </div>
-                    ))}  
+                     
                 </div>
             </div>
        
             <div id='AlbumResults'>
                 <div className='List'>
                     <p>Albums</p>
-                    {props.albumList.map((option, index) => (
-                        <div key={index}>
-                            <a>
-                                <span>{option.album}</span>
-                                <span>{option.artist}</span>
-                                <ViewButton 
-                                    number={2} 
-                                    value1={option.song}
-                                    value2={option.artist}
-                                    value3={option.album}
-                                    value={option.album}
-                                    display={props.display}
-                                    setDisplay={props.setDisplay}
-                                    Album={props.Album}
-                                    setAlbum={props.setAlbum}
-                                    setArtist={props.setArtist}
-                                    Artist={props.Artist}
-                                    NowPlaying={props.NowPlaying}
-                                    setNowPlaying={props.setNowPlaying}
-                                    thisPlaylist={props.thisPlaylist}
-                                    setThisPlaylist={props.setThisPlaylist}/>
-                            </a>
-                        </div>
-                    ))}
+                    
            
                 </div>
             </div>
